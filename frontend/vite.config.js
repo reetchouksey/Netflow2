14d2164df54800e546565c19794cc0866a456aa8
Mohd Aman <105653775+mramankhann@users.noreply.github.com>
2026-08-26 03:26:02 -0700

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/docs': {
-        target: 'http://localhost:5174',
+        target: 'http://[::1]:5174',
         changeOrigin: true
       }
     }
